383ec63ef730e97c5d3371c06d694fb3958bf7a2
Robertoarce <roberto_arce_@hotmail.com>
2025-03-28 13:53:03 +0100

    adding sample training for fast iteration

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 
-def dbscan_scorer(estimator, X):
+def dbscan_scorer_before(estimator, X):
     #use labels
     labels = estimator.named_steps['dbscan'].labels_
     
@@ -21,3 +21,28 @@ def dbscan_scorer(estimator, X):
     else:
         # Return -1 to show no cluster is found
         return -1
+    
+
+def dbscan_scorer(estimator, X, labels):
+    # Count of non-noise points
+    n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise = list(labels).count(-1)
+    
+    # If all points are noise or only one cluster, return a bad score
+    if n_clusters <= 1:
+        return -1.0
+    
+    # Calculate silhouette score only on non-noise points if there are any
+    if n_noise < len(labels):
+        # Create a mask for non-noise points
+        mask = labels != -1
+        if sum(mask) > 1:  # Need at least 2 points for silhouette
+            try:
+                score = silhouette_score(X[mask], labels[mask])
+                # We want fewer noise points for same silhouette
+                noise_penalty = n_noise / len(labels)
+                return score * (1 - noise_penalty)
+            except:
+                return -1.0
+    
+    return -1.0
